f847113c1b70855fa4d34c3bbdb3b24a4c3082ce
FelixBeauX54 <beaulieu.felixcr76@gmail.com>
2024-04-22 16:27:44 -0400

    Ajustement retour à l'accueil dans le 500

--- front-end/projet_application/src/routes/500/+page.svelte
+++ front-end/projet_application/src/routes/500/+page.svelte
@@ -6,6 +6,7 @@
     <h1>500</h1>
     <h2>Un problème est survenu de notre côté...</h2>
     <p>Nous serons de retour bientôt</p>
+    <a href="/">Retour à l'accueil</a>
 </div>
 
 <style scoped>
@@ -29,6 +30,13 @@
     p {
         font-size: 1.2em;
         margin: 0;
+        color: #fff;
+        margin: 5px;
+    }
+    a {
+        color: #00ad9a;
+    }
+    a:hover {
         color: #fff;
     }
 </style>
