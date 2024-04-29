2f177ad9f2d56815f083b883b56f17bbf0a6cfc8
Rino Gamache <2035718@cegeprdl.ca>
2024-04-29 14:03:04 -0400

    Petite modif

--- front-end/projet_application/src/Models/Offre.ts
+++ front-end/projet_application/src/Models/Offre.ts
@@ -13,7 +13,7 @@ export interface jobOffer {
     offerLink: string; // lien vers l'offre ou site web de l'employeur
     offerStatus: number;
     active: boolean; // si l'offre est active ou non
-    salary: number; // salaire de lheure                     ** A AJOUTER BD **
+    salary: string; // salaire de lheure                     ** A AJOUTER BD **
     scheduleId: number; // id de l'horaire de travail
     employerId: number; // id de l'employeur 
     isApproved: boolean; // si l'offre est approuvée ou non
