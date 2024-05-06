dcb504cd444b595a6f4486e247af7d02b08b250d
Zachary Lemieux <zacha21lem@hotmail.com>
2024-05-06 10:01:40 -0400

    Bug regler

--- front-end/projet_application/src/routes/emplois/+page.svelte
+++ front-end/projet_application/src/routes/emplois/+page.svelte
@@ -1,112 +1,112 @@
 <script lang="ts">
-    import "../../styles/global.css";
-    import Header from "../../Components/Common/Header.svelte";
-    import Footer from "../../Components/Common/Footer.svelte";
-    import EmploiRow from "../../Components/OffreEmplois/EmploiRow.svelte";
-    import OffreEmploi from "../../Components/OffreEmplois/OffreEmploi.svelte";
-    import { writable } from "svelte/store";
-    import type { jobOffer } from "../../Models/Offre";
-    import { GET } from "../../ts/server";
-    import { onMount } from "svelte";
+  import "../../styles/global.css";
+  import Header from "../../Components/Common/Header.svelte";
+  import Footer from "../../Components/Common/Footer.svelte";
+  import EmploiRow from "../../Components/OffreEmplois/EmploiRow.svelte";
+  import OffreEmploi from "../../Components/OffreEmplois/OffreEmploi.svelte";
+  import { writable } from "svelte/store";
+  import type { jobOffer } from "../../Models/Offre";
+  import type { Entreprise } from "../../Models/Entreprise";
+  import { GET } from "../../ts/server";
+  import { onMount } from "svelte";
   import Modal from "../../Components/Common/Modal.svelte";
 
-    const modal = writable(false);
-    const selectedEmploiId = writable(0);
-    const openModal = (id: number) => {
-        modal.set(true);
-        selectedEmploiId.set(id);
-    };
-    const closeModal = () => {
-        modal.set(false);
-    };
-    const handleEmploiClick = (offreId: number) => {
-        openModal(offreId);
-    };
+  const modal = writable(false);
+  const selectedEmploiId = writable(0);
+  const openModal = (id: number) => {
+    modal.set(true);
+    selectedEmploiId.set(id);
+  };
+  const closeModal = () => {
+    modal.set(false);
+  };
+  const handleEmploiClick = (offreId: number) => {
 
-    const jobOffers = writable<jobOffer[]>([]);
-    const getJobOffers = async () => {
-        try {
-            const response = await GET<any>("/jobOffer/offresEmploiApproved");
-            jobOffers.set(response);
-        } catch (error) {
-            console.error("Error fetching job offers:", error);
-        }
-    };
-    onMount(getJobOffers);
+    openModal(offreId);
+  };
+
+  const jobOffers = writable<jobOffer[]>([]);
+  const getJobOffers = async () => {
+    try {
+      const response = await GET<any>("/jobOffer/offresEmploiApproved");
+      jobOffers.set(response);
+    } catch (error) {
+      console.error("Error fetching job offers:", error);
+    }
+  };
+  onMount(getJobOffers);
 </script>
 
 <Header />
 <main>
-    <section class="haut">
-        <div class="haut-gauche">
-            <h1 class="title">
-                <span class="text">EMPLOIS </span><span class="text">
-                    DISPONIBLES</span
-                >
-            </h1>
-        </div>
-    </section>
-    <section class="offres">
-        {#each $jobOffers as offre}
-            <EmploiRow offre={offre} handleModalClick={handleEmploiClick}/>
-        {/each}
-    </section>
-    {#if $modal}
-        {#each $jobOffers as emploi}
-            {#if emploi.id === $selectedEmploiId}
-            <Modal handleModalClick={closeModal}>
-                <OffreEmploi offre={emploi} />
-            </Modal>
-            {/if}
-        {/each}
-    {/if}
+  <section class="haut">
+    <div class="haut-gauche">
+      <h1 class="title">
+        <span class="text">EMPLOIS </span><span class="text"> DISPONIBLES</span>
+      </h1>
+    </div>
+  </section>
+  <section class="offres">
+    {#each $jobOffers as offre}
+      <EmploiRow {offre} handleModalClick={handleEmploiClick} />
+    {/each}
+  </section>
+  {#if $modal}
+    {#each $jobOffers as emploi}
+      {#if emploi.id === $selectedEmploiId}
+          <Modal handleModalClick={closeModal}>
+            <OffreEmploi offre={emploi}/>
+          </Modal>
+      {/if}
+    {/each}
+  {/if}
 </main>
 <Footer />
 
 <style scoped>
-    .title {
-        left: 7.2%;
-        margin: 0;
-        margin-top: 30px;
-    }
-    .title span:first-child {
-        color: white;
-        margin: 0;
-    }
-    .title span:last-child {
-        color: #00ad9a;
-        margin: 0;
-    }
-    .text {
-        font-size: 2.5vw;
-        margin: 0;
-    }
-    main {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-    }
-    .haut {
-        display: flex;
-        width: 85%;
-        margin-bottom: 30px;
-    }
-    .haut-gauche {
-        display: flex;
-        flex-direction: column;
-        width: 50%;
-        margin-left: 5.2%;
-    }
-    .offres {
-        width: fit-content;
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-    }
-    .offres {
-        width: fit-content;
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-    }
+  .title {
+    left: 7.2%;
+    margin: 0;
+    margin-top: 30px;
+  }
+  .title span:first-child {
+    color: white;
+    margin: 0;
+  }
+  .title span:last-child {
+    color: #00ad9a;
+    margin: 0;
+  }
+  .text {
+    font-size: 2.5vw;
+    margin: 0;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  .haut {
+    display: flex;
+    width: 85%;
+    margin-bottom: 30px;
+  }
+  .haut-gauche {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin-left: 5.2%;
+  }
+  .offres {
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+  .offres {
+    width: fit-content;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
 </style>
