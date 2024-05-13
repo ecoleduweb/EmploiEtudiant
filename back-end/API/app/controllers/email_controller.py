6f66d97930e0173c1179f89e5f07f4eb59dd8dfd
antoineCegepRDL <127795114+antoineCegepRDL@users.noreply.github.com>
2024-05-13 14:51:26 -0400

    Update requirements and removed email route to api (#160)
    * Added mail script to test and requirments line
    
    * removed controller to prevent unwanted email sent

--- back-end/API/app/controllers/email_controller.py
+++ back-end/API/app/controllers/email_controller.py
@@ -13,29 +13,29 @@ password = os.environ.get('MAIL_SERVER_PASSWORD')
 
 def sendMail(receiver_email, subject, content):
 
-    subject = "Site de recrutement - " + subject
-    html = """\
-    <html>
-    <body>
-        <p>""" + content + """</p>
-    </body>
-    </html>
-    """
+    # subject = "Site de recrutement - " + subject
+    # html = """\
+    # <html>
+    # <body>
+    #     <p>""" + content + """</p>
+    # </body>
+    # </html>
+    # """
 
-    # Create a multipart message and set headers
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = subject
+    # # Create a multipart message and set headers
+    # message = MIMEMultipart()
+    # message["From"] = sender_email
+    # message["To"] = receiver_email
+    # message["Subject"] = subject
 
-    # Attach the HTML part
-    message.attach(MIMEText(html, "html"))
+    # # Attach the HTML part
+    # message.attach(MIMEText(html, "html"))
 
-    # Send the email
-    with smtplib.SMTP(smtp_server, port) as server:
-        server.connect(smtp_server, port)
-        server.starttls()
-        server.login(login, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
-        server.quit()
+    # # Send the email
+    # with smtplib.SMTP(smtp_server, port) as server:
+    #     server.connect(smtp_server, port)
+    #     server.starttls()
+    #     server.login(login, password)
+    #     server.sendmail(sender_email, receiver_email, message.as_string())
+    #     server.quit()
     return True
