1de2836ecaf8eee1cc14748b438a06f16dc877f9
MarcAntoineLavoie23 <2348598@cegeprdl.ca>
2024-06-25 15:18:32 -0400

    Fini?

--- front-end/projet_application/src/routes/+page.svelte
+++ front-end/projet_application/src/routes/+page.svelte
@@ -8,14 +8,45 @@
     import type { JobOffer } from "../Models/Offre"
     import { GET } from "../ts/server"
     import { onMount } from "svelte"
+    import { error } from "@sveltejs/kit"
 
     let loaded = false
 
     const jobOffers = writable<JobOffer[]>([])
+    let LatestJobOffers: JobOffer[] = []
     const getJobOffers = async () => {
         try {
             const response = await GET<any>("/jobOffer/approved")
             jobOffers.set(response)
+
+            let JobOffersLen = $jobOffers.length
+
+            if (JobOffersLen > 0) //Vérifie s'il y a des éléments dans l'array
+            {
+                if (JobOffersLen > 5) //Il y a plus que 5
+                {
+                    //Cherche le dernier job offer et recupère ça date et vérifie s'il est plus que 7 jours (s'il est plus grand que 7, ça vas être 7)
+                    let OldestJobOfferDate = new Date($jobOffers[ ((JobOffersLen >= 7 ? 7 : 6) - 1) ].approvedDate).getTime()
+                        < new Date().getTime()
+
+                    if (JobOffersLen > 7 && OldestJobOfferDate) //S'il y en à plus que 7 et que la date, donne juste 7 offers
+                    {
+                        LatestJobOffers = ($jobOffers).slice(0, 7)
+                    }
+                    else if (JobOffersLen <= 7 && OldestJobOfferDate) //S'il y a 6
+                    {
+                        LatestJobOffers = ($jobOffers).slice(0, JobOffersLen)
+                    }
+                    else //Si la date est plus grand que 7 jour donc 5
+                    {
+                        LatestJobOffers = ($jobOffers).slice(0, 5)
+                    }
+                }
+                else //Il n'y a pas plus que 5
+                {
+                    LatestJobOffers = ($jobOffers).slice(0, JobOffersLen)
+                }
+            }
         } catch (error) {
             console.error("Error fetching job offers:", error)
         }
@@ -33,6 +64,10 @@
 
         finally
         {
+            $jobOffers.forEach(element => {
+                console.log("Offer Name:" + element.title + " Approbation Date:" + new Date(element.approvedDate))
+            });
+            
             loaded = true
         }
     })
@@ -73,7 +108,7 @@
         </section>
     {:else}
         <section class="offres">
-            {#each ($jobOffers).slice(0, 5) as offer}
+            {#each LatestJobOffers as offer}
                 <DetailOfferRow {offer} 
                 handleModalClick={(function() {})}/>
             {/each}
