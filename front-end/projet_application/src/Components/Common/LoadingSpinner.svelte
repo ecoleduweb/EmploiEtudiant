278c9c8e2f53b5d56a64ebcf8969947d4062f6fe
MarcAntoineLavoie23 <157745159+MarcAntoineLavoie23@users.noreply.github.com>
2024-06-26 19:42:00 -0400

    Réduit la taille du chargement (#185)
    Réduit 2x la taille du chargement

--- front-end/projet_application/src/Components/Common/LoadingSpinner.svelte
+++ front-end/projet_application/src/Components/Common/LoadingSpinner.svelte
@@ -1,6 +1,9 @@
-<script lang="ts">
-
-</script>
-  
-  
 <img src="Loading_logo_cegepRDL.gif" alt="Logo cegep chargement">
+
+<style scoped>
+    img 
+    {
+        width: 125px;
+        height: 125px;
+    }
+</style>
