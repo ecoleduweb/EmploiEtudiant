7a0621189054ae29e8b68a719cfd961df3187dff
MarcAntoineLavoie23 <2348598@cegeprdl.ca>
2024-07-23 15:02:48 -0400

    Fix

--- front-end/projet_application/src/Components/JobOffer/CreateEditJobOffer.svelte
+++ front-end/projet_application/src/Components/JobOffer/CreateEditJobOffer.svelte
@@ -11,6 +11,7 @@
     import { onMount } from "svelte"
     import { currentUser, isLoggedIn, studyPrograms } from "$lib"
     import fetchCity from "../../Service/CityService"
+    import EntrepriseDetails from "./EntrepriseDetails.svelte"
     export let isJobOfferEdit: boolean
 
     // valeur par défaut de l'offer utilisée pour le create.
@@ -329,38 +330,7 @@
             {:else}
                 {#if isEnterpriseSelected}
                     <h1>Création d'une nouvelle entreprise</h1>
-                    <div class="form-group-vertical">
-                        <label for="title">Nom*</label>
-                        <p>{enterprise.name}</p>
-                    </div>
-                    <div class="form-group-vertical">
-                        <label for="schedule">Adresse*</label>
-                        <p>{enterprise.address}</p>
-                    </div>
-                    <div class="form-group-vertical">
-                        <label for="lieu">Courriel*</label>
-                        <p>{enterprise.email}</p>
-                    </div>
-                    <div class="form-group-vertical">
-                        <label for="lieu">Téléphone*</label>
-                        <p>{enterprise.phone}</p>
-                    </div>
-                    <div class="form-group-vertical">
-                        <label for="lieu">Ville*</label>
-                        {#if cityOptions.length === 0}
-                            <p>Chargement des villes...</p>
-                        {:else}
-                            <MultiSelect
-                                id="ville"
-                                options={cityOptions}
-                                closeDropdownOnSelect={true}
-                                placeholder="Choisir ville..."
-                                bind:value={selectedCity}
-                                bind:selected={cityFromEnterprise}
-                                disabled={false}
-                            />
-                        {/if}
-                    </div>
+                    <EntrepriseDetails {enterprise} {cityOptions} {selectedCity} {cityFromEnterprise}></EntrepriseDetails>
                 {:else}
                     <h1>Création d'une nouvelle entreprise</h1>
                     <div class="form-group-vertical">
@@ -427,7 +397,6 @@
                                 placeholder="Choisir ville..."
                                 bind:value={selectedCity}
                                 bind:selected={cityFromEnterprise}
-                                disabled={!isJobOfferEdit && isEnterpriseSelected}
                             />
                         {/if}
                     </div>
@@ -436,113 +405,10 @@
 
             <h1>Création d'une nouvelle offre d'emploi</h1>
         {:else}
-            {#if isEnterpriseSelected}
-                <h1>Modification d'une entreprise</h1>
-                <div class="form-group-vertical">
-                    <label for="title">Nom*</label>
-                    <p>{enterprise.name}</p>
-                </div>
-                <div class="form-group-vertical">
-                    <label for="schedule">Adresse*</label>
-                    <p>{enterprise.address}</p>
-                </div>
-                <div class="form-group-vertical">
-                    <label for="lieu">Courriel*</label>
-                    <p>{enterprise.email}</p>
-                </div>
-                <div class="form-group-vertical">
-                    <label for="lieu">Téléphone*</label>
-                    <p>{enterprise.phone}</p>
-                </div>
-                <div class="form-group-vertical">
-                    <label for="lieu">Ville*</label>
-                    {#if cityOptions.length === 0}
-                        <p>Chargement des villes...</p>
-                    {:else}
-                        <MultiSelect
-                            id="ville"
-                            options={cityOptions}
-                            closeDropdownOnSelect={true}
-                            placeholder="Choisir ville..."
-                            bind:value={selectedCity}
-                            bind:selected={cityFromEnterprise}
-                            disabled={false}
-                        />
-                    {/if}
-                </div>
-            {:else}
-                <h1>Modification d'une entreprise</h1>
-                <div class="form-group-vertical">
-                    <label for="title">Nom*</label>
-                    <input
-                        type="text"
-                        bind:value={enterprise.name}
-                        class="form-control"
-                        id="titre"
-                        readonly={!isJobOfferEdit && isEnterpriseSelected}
-                    />
-                </div>
-                <p class="errors-input">
-                    {#if errorsEnterprise.name}{errorsEnterprise.name}{/if}
-                </p>
-                <div class="form-group-vertical">
-                    <label for="schedule">Adresse*</label>
-                    <input
-                        type="text"
-                        bind:value={enterprise.address}
-                        class="form-control"
-                        id="address"
-                        readonly={!isJobOfferEdit && isEnterpriseSelected}
-                    />
-                    </div>
-                    <p class="errors-input">
-                        {#if errorsEnterprise.address}{errorsEnterprise.address}{/if}
-                    </p>
-                    <div class="form-group-vertical">
-                        <label for="lieu">Courriel*</label>
-                        <input
-                            type="text"
-                            bind:value={enterprise.email}
-                            class="form-control"
-                            id="email"
-                            readonly={!isJobOfferEdit && isEnterpriseSelected}
-                    />
-                </div>
-                <p class="errors-input">
-                    {#if errorsEnterprise.email}{errorsEnterprise.email}{/if}
-                </p>
-                <div class="form-group-vertical">
-                    <label for="lieu">Téléphone*</label>
-                    <input
-                        type="text"
-                        bind:value={enterprise.phone}
-                        class="form-control"
-                        id="phone"
-                        readonly={!isJobOfferEdit && isEnterpriseSelected}
-                    />
-                </div>
-                <p class="errors-input">
-                    {#if errorsEnterprise.phone}{errorsEnterprise.phone}{/if}
-                </p>
-                <div class="form-group-vertical">
-                    <label for="lieu">Ville*</label>
-                    {#if cityOptions.length === 0}
-                        <p>Chargement des villes...</p>
-                    {:else}
-                        <MultiSelect
-                            id="ville"
-                            options={cityOptions}
-                            closeDropdownOnSelect={true}
-                            placeholder="Choisir ville..."
-                            bind:value={selectedCity}
-                            bind:selected={cityFromEnterprise}
-                            disabled={!isJobOfferEdit && isEnterpriseSelected}
-                        />
-                {/if}
-                </div>
+            <h1>Modification d'une entreprise</h1>
+            <EntrepriseDetails {enterprise} {cityOptions} {selectedCity} {cityFromEnterprise}></EntrepriseDetails>
 
-                <h1>Modification d'une offre d'emploi</h1>
-            {/if}
+            <h1>Modification d'une offre d'emploi</h1>
         {/if}
 
         <div class="form-group-vertical">
