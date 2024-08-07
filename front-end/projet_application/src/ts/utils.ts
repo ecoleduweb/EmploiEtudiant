4e2e4caf46ed46c0e78414f779cd5e71eff2e7ee
Antoine Chagnon Michaud <achagnonmichaud@cvm.qc.ca>
2024-08-06 20:10:57 -0400

    Fixed max and min date error

--- front-end/projet_application/src/ts/utils.ts
+++ front-end/projet_application/src/ts/utils.ts
@@ -4,3 +4,10 @@ export const extractErrors = (err: ErrorResponse | any) => {
         return { ...acc, [err.path]: err.message }
     }, {})
 }
+
+export const toFormattedDateString = (date: Date): string => {
+    const year = date.getFullYear()
+    const month = String(date.getMonth() + 1).padStart(2, '0')
+    const day = String(date.getDate()).padStart(2, '0')
+    return `${year}-${month}-${day}`
+}
