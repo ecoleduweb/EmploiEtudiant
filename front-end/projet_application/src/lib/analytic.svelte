f128c0ee6f3c2cdbf22e16ba02f819afa7aed5cd
Rino Gamache <2035718@cegeprdl.ca>
2024-11-25 21:13:43 -0500

    Update analytic.svelte

--- front-end/projet_application/src/lib/analytic.svelte
+++ front-end/projet_application/src/lib/analytic.svelte
@@ -3,19 +3,18 @@
   import { env } from "$env/dynamic/public"
   
 
-  const measurementId = env.PUBLIC_PUBLIC_MEASUREMENT_ID
+  const measurementId: string | undefined = env.PUBLIC_MEASUREMENT_ID;
 
-  console.log("Measurement ID:", measurementId); // Ajoutez cette ligne pour vérifier la valeur de measurementId
+  console.log("Measurement ID:", measurementId); 
 
   $: {
     if (typeof gtag !== 'undefined') {
-      console.log("gtag is defined"); // Vérifiez si gtag est défini
       gtag('config', measurementId, {
         page_title: document.title,
         page_path: $page.url.pathname,
       })
     } else {
-      console.log("gtag is not defined"); // Vérifiez si gtag n'est pas défini
+      console.warn('gtag is not defined')
     }
   }
 </script>
