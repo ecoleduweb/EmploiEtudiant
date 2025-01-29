d546469b49d84f1f1138133deaf8d59fde840a8e
Steve <124377517+2265857@users.noreply.github.com>
2025-01-29 16:41:56 -0500

    Ajout des indications "de l'entreprise" au label lors de la créatio… (#270)
    * • Ajout des indications "de l'entreprise" au label lors de la création d'une entreprise
    
    * Correction pull request globall.css
    
    ---------
    
    Co-authored-by: 2265857 <steve__l@hotmail.com>

--- front-end/projet_application/src/Components/JobOffer/CreateEditJobOffer.svelte
+++ front-end/projet_application/src/Components/JobOffer/CreateEditJobOffer.svelte
@@ -359,20 +359,20 @@
                 {/if}
             {:else}
                 {#if isEnterpriseSelected}
-                    <h1>Création d'une nouvelle entreprise</h1>
+                    <h1>Création d'une nouvelle <span class="hightlight">entreprise</span></h1>
                     <EntrepriseDetails {enterprise} {selectedCity} ></EntrepriseDetails>
                 {:else}
-                    <h1>Création d'une nouvelle entreprise</h1>
+                    <h1>Création d'une nouvelle <span class="hightlight">entreprise</span></h1>
                     <CreateEditEnterprise {enterprise} errorsEnterprise={enterpriseErrors} {cityOptions} selectedCity={selectedCityWritable} {cityFromEnterprise} ></CreateEditEnterprise>
                 {/if}
             {/if}
 
-            <h1>Création d'une nouvelle offre d'emploi</h1>
+            <h1>Création d'une nouvelle <span class="hightlight">offre d'emploi</span></h1>
         {:else}
             <h1>Mon entreprise</h1>
             <EntrepriseDetails {enterprise} {selectedCity} ></EntrepriseDetails>
 
-            <h1>Modification d'une offre d'emploi</h1>
+            <h1>Modification d'une <span class="hightlight">offre d'emploi</span></h1>
         {/if}
 
         <div class="form-group-vertical">
