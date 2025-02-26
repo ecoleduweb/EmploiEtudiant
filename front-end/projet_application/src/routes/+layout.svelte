d207b232341e1b77c2371b21eef8c677f37ebf2a
JeanFelixLarouche <2238092@cegeprdl.ca>
2025-02-26 16:00:05 -0500

    Ajout d'un espace de la hauteur du footer pour éviter de cacher la bas du main

--- front-end/projet_application/src/routes/+layout.svelte
+++ front-end/projet_application/src/routes/+layout.svelte
@@ -6,21 +6,27 @@
 </script>
 
 
+<div class="container">
+    <Header/>
+    <CookieBanner />
 
-<Header/>
-<CookieBanner />
+    <main class="content">
+        <slot />
+    </main>
 
-<slot />
-
-<div class="FooterDiv">
+    <div class="footer-spacer"></div>
     <Footer/>
 </div>
 
 <style lang="scss" scoped>
-    .FooterDiv 
-    {
-        position: fixed;
-        bottom: 0;
-        min-width: 100%;
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+    .content {
+        flex: 1;
+    }
+    .footer-spacer {
+        height: 3.01vw
     }
 </style>
