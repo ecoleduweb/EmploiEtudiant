64468b3519af07be767819828daa0e635b609acf
JeanFelixLarouche <2238092@cegeprdl.ca>
2025-03-12 15:44:19 -0400

    Ajout de la télémétrie frontend avec signoz + Variables d'environnement pour le déploiement

--- front-end/projet_application/src/routes/+layout.svelte
+++ front-end/projet_application/src/routes/+layout.svelte
@@ -3,22 +3,26 @@
     import Header from "../Components/Common/Header.svelte"
     import CookieBanner from "../Components/Common/CookieBanner.svelte"
     import Footer from "../Components/Common/Footer.svelte"
+    import { ClientTelemetry } from "$lib/tracer"
+    const ENABLED_TELEMETRY = import.meta.env.VITE_ENABLED_TELEMETRY || false //Garder à false en développement local
+
+    if (ENABLED_TELEMETRY) {
+        const telemetry = ClientTelemetry.getInstance()
+        telemetry.start()
+    }
 </script>
 
-
-
-<Header/>
+<Header />
 <CookieBanner />
 
 <slot />
 
 <div class="FooterDiv">
-    <Footer/>
+    <Footer />
 </div>
 
 <style lang="scss" scoped>
-    .FooterDiv 
-    {
+    .FooterDiv {
         position: fixed;
         bottom: 0;
         min-width: 100%;
