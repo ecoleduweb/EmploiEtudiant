9b664766da273c3d8e5cf5c3430a986615b1a66e
2265857 <steve__l@hotmail.com>
2025-03-12 16:46:48 -0400

    Tri pour les dates dans Dashboard - section: Offres en attente d'approbation

--- front-end/projet_application/src/routes/dashboard/+page.svelte
+++ front-end/projet_application/src/routes/dashboard/+page.svelte
@@ -125,6 +125,7 @@
     let dateNow = new Date().toISOString().split("T")[0]
 
     $: toBeApprovedOffer = $jobOffers.filter((x) => x.isApproved === null)
+        .sort((a, b) => new Date(a.offerDebut).getTime() - new Date(b.offerDebut).getTime());
     $: isRefusedOffer = $jobOffers.filter((x) => x.isApproved === false)
     $: offerToCome = $jobOffers.filter((x) => {
         if (!x.isApproved) return false
