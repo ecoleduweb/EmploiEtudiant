97c0fea37bbac085e5ed227bfefb5fabe57f7a69
JeanFelixLarouche <124377533+JeanFelixLarouche@users.noreply.github.com>
2025-03-20 18:45:34 -0400

    Signoz - Partie frontend (#308)
    * Ajout de la télémétrie frontend avec signoz + Variables d'environnement pour le déploiement
    
    * Ajout de requirements
    
    * Correction de dépendance

--- front-end/projet_application/src/routes/+layout.svelte
+++ front-end/projet_application/src/routes/+layout.svelte
@@ -3,8 +3,14 @@
     import Header from "../Components/Common/Header.svelte"
     import CookieBanner from "../Components/Common/CookieBanner.svelte"
     import Footer from "../Components/Common/Footer.svelte"
-</script>
+    import { ClientTelemetry } from "$lib/tracer"
+    const ENABLED_TELEMETRY = import.meta.env.VITE_ENABLED_TELEMETRY || true //Garder à false en développement local
 
+    if (ENABLED_TELEMETRY) {
+        const telemetry = ClientTelemetry.getInstance()
+        telemetry.start()
+    }
+</script>
 
 <div class="container">
     <Header/>
