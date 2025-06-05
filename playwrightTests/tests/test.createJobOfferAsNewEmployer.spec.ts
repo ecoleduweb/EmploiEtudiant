b45bcf8c2fc605883221ad7203499c80f0c903b2
VincentBouchar-d <124377262+VincentBouchar-d@users.noreply.github.com>
2025-06-05 14:38:41 -0400

    Ajustement des dates limite pour postuler pour toujours être dans le … (#350)
    * Ajustement des dates limite pour postuler pour toujours être dans le futur
    
    * Ajout du page.clock et redéfinition des mocks

--- playwrightTests/tests/test.createJobOfferAsNewEmployer.spec.ts
+++ playwrightTests/tests/test.createJobOfferAsNewEmployer.spec.ts
@@ -11,6 +11,8 @@ import { enterpriseMocks } from '../Helper/Mocks/enterprise.mock';
 
 test.describe('createNewJobOffer', () => {
   var apiMocker;
+  const nextWeekDateFormatted = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+
   test.beforeEach(async ({ page }) => {
     apiMocker = new ApiMocker(page);
     await apiMocker.addMocks([
@@ -22,6 +24,7 @@ test.describe('createNewJobOffer', () => {
       jobOfferMocks.jobOfferEmployer,
       enterpriseMocks.notFound])
       .apply();
+    await page.clock.install({ time: new Date('2016-02-25T08:00:00-04:00') });
 
     // se connecte au site (ADDRESSE A CHANGER LORSQUE LE SITE SERA DÉPLOYÉ)
     await page.goto('http://localhost:5002/dashboard');
@@ -51,7 +54,7 @@ test.describe('createNewJobOffer', () => {
     await page.getByRole('option', { name: 'temps plein' }).click();
     await page.locator('#address').nth(1).click();
     await page.locator('#address').nth(1).fill('Addresse Lieu 123');
-    await page.getByLabel('Date limite pour postuler*').fill('2025-05-13');
+    await page.getByLabel('Date limite pour postuler*').fill('2016-03-01');
     await page.getByPlaceholder('Choisir programme(s)').click();
     await page.getByRole('option', { name: 'Arts visuels' }).click();
     await page.getByLabel('Salaire Horaire').click();
@@ -181,7 +184,7 @@ test.describe('createNewJobOffer', () => {
     await page.getByRole('option', { name: 'temps plein' }).click();
     await page.locator('#address').nth(1).click();
     await page.locator('#address').nth(1).fill('Addresse Lieu 123');
-    await page.getByLabel('Date limite pour postuler*').fill('2025-05-13');
+    await page.getByLabel('Date limite pour postuler*').fill('2016-03-01');
     await page.getByPlaceholder('Choisir programme(s)').click();
     await page.getByRole('option', { name: 'Arts visuels' }).click();
     await page.getByLabel('Salaire Horaire').click();
@@ -220,7 +223,7 @@ test.describe('createNewJobOffer', () => {
     await page.getByRole('option', { name: 'temps plein' }).click();
     await page.locator('#address').nth(1).click();
     await page.locator('#address').nth(1).fill('Addresse Lieu 123');
-    await page.getByLabel('Date limite pour postuler*').fill('2025-05-13');
+    await page.getByLabel('Date limite pour postuler*').fill('2016-03-01');
     await page.getByPlaceholder('Choisir programme(s)').click();
     await page.getByRole('option', { name: 'Arts visuels' }).click();
     await page.getByLabel('Salaire Horaire').click();
--- playwrightTests/tests/test.createUpdateJobOfferBackEndError.spec.ts
+++ playwrightTests/tests/test.createUpdateJobOfferBackEndError.spec.ts
@@ -24,6 +24,7 @@ test.describe('createUpdateJobOfferBackEndError', () => {
       jobOfferMocks.jobOfferNew,
       enterpriseMocks.success])
       .apply();
+    await page.clock.install({ time: new Date('2016-02-25T08:00:00-04:00') });
   });
 
   test('Nouvelle offre invalide', async ({ page }) => {
